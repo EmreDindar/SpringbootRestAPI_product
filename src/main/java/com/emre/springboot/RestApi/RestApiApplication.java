d30d1f632ff47ab233381a09f570276ef11217f7
Emre Dindar <emredindar93@hotmail.com>
2020-01-29 13:47:14 +0300

    push trial

--- src/main/java/com/emre/springboot/RestApi/RestApiApplication.java
+++ src/main/java/com/emre/springboot/RestApi/RestApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApiApplication {
 
 	public static void main(String[] args) {
-		//git push deneme
+
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
